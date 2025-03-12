7914fc035578283b691cc3623ed20fe50a78f2cf
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2025-03-12 19:26:02 +0800

    Add swagger can change port

--- AIJobCareer/Program.cs
+++ AIJobCareer/Program.cs
@@ -6,11 +6,16 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Configure Swagger/OpenAPI
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1"));
     app.MapOpenApi();
 }
 
